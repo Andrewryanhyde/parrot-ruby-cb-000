934452b7ff876068b661c74b010775c1c99a392a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-15 07:53:50 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string) 
-  puts string 
-  return string 
-end 
+def parrot(string = "Squawk!")
+  puts string
+  return string
+end
